f58ba76c039d59f7f41b90c6adb7aec4a49e75e8
Dishu Bagga <dishubagga9111@gmail.com>
2023-08-02 11:24:19 +0200

    Added route and intial setup

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { Pets } from "./components/pets/Pets";
 
 const App = () => {
-  return <Pets />;
+  return (<>
+  <h1>Welcome to react app</h1>
+  </>);
 };
 
 export default App;
