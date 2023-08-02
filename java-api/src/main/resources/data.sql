7cb48b97c4a8af484079e7da714242849df4a4d9
kpg123 <48265598+kpg123@users.noreply.github.com>
2023-08-02 11:36:00 -0400

    Update data.sql

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -1,9 +1,9 @@
-INSERT INTO tableName (book_name) VALUES ('trading_book_1');
-INSERT INTO tableName (book_name) VALUES ('trading_book_2');
-INSERT INTO tableName (book_name) VALUES ('Trading_book_3');
-INSERT INTO tableName (book_name) VALUES ('Trading_book_2');
-INSERT INTO tableName (book_name) VALUES ('Trading_book_4');
-INSERT INTO tableName (book_name) VALUES ('Trading_book_6');
+INSERT INTO book (bookName) VALUES ('trading_book_1');
+INSERT INTO book (bookName) VALUES ('trading_book_2');
+INSERT INTO book (bookName) VALUES ('Trading_book_3');
+INSERT INTO book (bookName) VALUES ('Trading_book_2');
+INSERT INTO book (bookName) VALUES ('Trading_book_4');
+INSERT INTO book (bookName) VALUES ('Trading_book_6');
 
 INSERT INTO counter_party (bond_holder) VALUES ('AZ Holdings Inc');
 INSERT INTO counter_party (bond_holder) VALUES ('Acme co');
