9e48a154165d89f22af20baf3d5cf535fc9aa166
masghar12 <m.asghar5@icloud.com>
2023-08-02 17:00:33 +0100

    first data row

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -1,3 +1,5 @@
-INSERT INTO dogs (dog_id, name, age) VALUES (1, 'Einstein', 3);
-INSERT INTO dogs (dog_id, name, age) VALUES (2, 'Kaya', 5);
-INSERT INTO dogs (dog_id, name, age) VALUES (3, 'Lassie', 7);
+
+
+INSERT INTO bonds (trade_type, trade_currency, quantity, trade_settlement_date, trade_status, trade_date, unit_price, coupon_percent, bond_currency, cusip, face_value, isin, issuer_name, bond_maturity_date, status, type, book_name, bond_holder) VALUES
+('buy','USD',50,	'2021-08-04',	'open',	'2021-05-13',	90,	4.37,	'USD',	1000,	'XS1988387210',	'BNPParibasIssu', 4.37, 'Microsoft Corp (USD)',	'2021-08-05',	'active',	'CORP',	'trading_book_1',	'AZ Holdings Inc');
+
