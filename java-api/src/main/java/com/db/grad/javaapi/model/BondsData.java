610473c6402bef68cb2018a59f9b87fcc7161a45
Thin Yati Su <thinyatihsu.hsu@gmail.com>
2023-08-02 15:30:18 +0100

    initial bonddata commit without getters and setters

--- java-api/src/main/java/com/db/grad/javaapi/model/BondsData.java
+++ java-api/src/main/java/com/db/grad/javaapi/model/BondsData.java
@@ -4,15 +4,32 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Date;
 
 @Entity
-@Table(name = "dogs")
-public class Dog
+@Table(name = "bondsdata")
+public class BondsData
 {
     @Id
-    private long id;
-    private String name;
-    private long age;
+    private long bondID;
+    private String tradeType;
+    private String tradeCurrency;
+    private Integer quantity;
+    private Date tradeSettlementDate;
+    private String tradeStatus;
+    private Date tradeDate;
+    private Double unitPrice;
+    private Double couponPercent;
+    private String bondCurrency;
+    private String cusip;
+    private Integer faceValue;
+    private String isin;
+    private String issuerName;
+    private Date bondMaturityDate;
+    private String status;
+    private String type;
+    private String bookName;
+    private String bondHolder;
 
     @Id
     @Column(name = "dog_id", nullable = false)
