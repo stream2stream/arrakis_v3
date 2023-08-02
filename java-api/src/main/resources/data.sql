0ca4ef565c334d070874744bb9474e43b5598b84
73359 <MichaelUhl1@gmx.de>
2023-08-02 12:54:00 +0100

    Table counterpartz, insert some values

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -1,3 +1,18 @@
 INSERT INTO dogs (dog_id, name, age) VALUES (1, 'Einstein', 3);
 INSERT INTO dogs (dog_id, name, age) VALUES (2, 'Kaya', 5);
 INSERT INTO dogs (dog_id, name, age) VALUES (3, 'Lassie', 7);
+
+INSERT INTO book (name) VALUES ('trading_book_1');
+INSERT INTO book (name) VALUES ('trading_book_2');
+INSERT INTO book (name) VALUES ('trading_book_3');
+INSERT INTO book (name) VALUES ('trading_book_4');
+INSERT INTO book (name) VALUES ('trading_book_5');
+INSERT INTO book (name) VALUES ('trading_book_6');
+
+INSERT INTO user (name, email) VALUES ('username', 'user@mail.de');
+
+INSERT INTO book_user (book_id, user_id) VALUES (3, 1);
+INSERT INTO book_user (book_id, user_id) VALUES (6, 1);
+
+INSERT INTO security (id, isin, cusip, issuer_name, maturity_date, coupon, type, face_value, currency, status) VALUES ('XS1988387210', '', 'BNPParibasIssu 4,37% Microsoft Corp (USD)', '2021-08-05', 4.37, 'CORP', 1000, 'USD', 'active');
+--INSERT INTO security (id, isin, cusip, issuer_name, maturity_date, coupon, type, face_value, currency, status) VALUES ('XS1988387210', '', 'BNPParibasIssu 4,37% Microsoft Corp (USD)', '2021-08-05', 4.37, 'CORP', 1000, 'GBP', 'active');
--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -14,8 +14,7 @@ CREATE TABLE book (
 CREATE TABLE user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(250) NOT NULL,
-    email VARCHAR(250) NOT NULL,
-    role VARCHAR(250) NOT NULL
+    email VARCHAR(250) NOT NULL
 );
 
 CREATE TABLE book_user (
@@ -23,35 +22,38 @@ CREATE TABLE book_user (
     user_id INT NOT NULL,
     FOREIGN key (book_id) REFERENCES book (id),
     FOREIGN key (user_id) REFERENCES user (id)
-)
+);
+
+CREATE TABLE trades (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    book_id INT NOT NULL,
+    security_id INT NOT NULL,
+    counterparty_id INT NOT NULL,
+    trade_type VARCHAR(50) NOT NULL,
+    trade_currency VARCHAR(10) NOT NULL,
+    quantity INT NOT NULL,
+    trade_settlement_date DATE NOT NULL,
+    trade_status VARCHAR(50) NOT NULL,
+    trade_date DATE NOT NULL,
+    unit_price FLOAT NOT NULL,
+    FOREIGN key (book_id) REFERENCES book (id),
+    FOREIGN key (security_id) REFERENCES security (id),
+    FOREIGN key (counterparty_id) REFERENCES counterparty (id),
+);
 
 CREATE TABLE security (
     id INT AUTO_INCREMENT PRIMARY KEY,
     isin VARCHAR(50),
     cusip VARCHAR(50),
     issuer_name VARCHAR(250) NOT NULL,
-    maturity_date DATE NOT NULL,
-    coupon FLOAT NOT NULL,
-    type VARCHAR(250) NOT NULL,
+    coupon_percent FLOAT NOT NULL,
+    bond_currency VARCHAR(10) NOT NULL,
     face_value FLOAT NOT NULL,
-    currency VARCHAR(10) NOT NULL,
-    status VARCHAR(50) NOT NULL
-)
+    maturity_date DATE NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    type VARCHAR(250) NOT NULL
+);
 
-CREATE TABLE trades (
+CREATE TABLE counterparty (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    book_id INT NOT NULL,
-    security_id INT NOT NULL,
---    counterparty_id INT NOT NULL,
-    currency VARCHAR(10) NOT NULL,
-    status VARCHAR(50) NOT NULL
-    quantity INT NOT NULL,
-    unit_price FLOAT NOT NULL,
-    buy_sell VARCHAR(50) NOT NULL,
-    trade_date DATE NOT NULL,
-    settlement_date DATE NOT NULL,
-    FOREIGN key (book_id) REFERENCES book (id),
-    FOREIGN key (security_id) REFERENCES security (id),
---    FOREIGN key (counterparty_id) REFERENCES counterparty (id),
-
 )
