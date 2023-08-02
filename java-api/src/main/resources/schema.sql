cc7b0b4436da06e5c1f5a98c146641822ec50382
ancamariac <ciuciuancamaria@gmail.com>
2023-08-02 13:24:56 +0300

    Init database structure

--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -1,8 +1,30 @@
-DROP TABLE IF EXISTS dogs;
+DROP TABLE IF EXISTS bonds;
 
-CREATE TABLE dogs (
-    dog_id INT NOT NULL,
-    name VARCHAR(250) NOT NULL,
-    age INT NOT NULL
+CREATE TABLE bonds (
+    isin VARCHAR(250) NOT NULL,
+    type VARCHAR(250) NOT NULL,
+    issuer_name VARCHAR(250) NOT NULL,
+    bond_maturity_date DATE NOT NULL,
+    face_value INT NOT NULL,
+    bond_currency VARCHAR(10) NOT NULL,
+    coupon_percent FLOAT NOT NULL,
+    status VARCHAR(100) NOT NULL,
+    cusip VARCHAR(100),
+    PRIMARY KEY (isin)
 );
 
+DROP TABLE IF EXISTS trades;
+
+CREATE TABLE trades (
+    isin VARCHAR(250) NOT NULL,
+    book_name VARCHAR(250) NOT NULL,
+    bond_holder VARCHAR(250) NOT NULL,
+    trade_status VARCHAR(20) NOT NULL,
+    unit_price FLOAT NOT NULL,
+    trade_currency VARCHAR(10) NOT NULL,
+    trade_type VARCHAR(10) NOT NULL,
+    trade_date DATE NOT NULL,
+    trade_settlement_date DATE NOT NULL,
+    quantity INT NOT NULL,
+    PRIMARY KEY (isin)
+);
