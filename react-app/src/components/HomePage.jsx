8e9f60467cf8c598e7d688d49d4cde06a1fce351
Daniel <djhenderson00@gmail.com>
2023-08-02 19:56:22 +0100

    Added the side card

--- react-app/src/components/HomePage.jsx
+++ react-app/src/components/HomePage.jsx
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
 // import { findPets } from "../../services/PetServices";
-// import { findSecurities } from "../../services/SecurityServices"
+import { findSecurities } from "../services/SecurityServices";
 import { DataGrid } from '@mui/x-data-grid';
 import { Box } from "@mui/material";
+import { Card } from "@mui/material";
+import CardContent from "@mui/material";
+import Typography from "@mui/material";
+import styles from "./pets/Pets.module.css";
 
 
 export const HomePage = () => {
@@ -70,11 +74,17 @@ export const HomePage = () => {
 
   return (
     <>
-          <Box sx={{ height: 400, width: '75%'}}>
+          <Box sx={{ height: '100%', width: '100%'}}>
+            <div className={styles.container}>
             <DataGrid
               rows={rowDefDev}
               columns={columnDef}
+              sx={{ maxWidth: '75%' }}
               />
+            <Card sx={{ minWidth: '25%'}}>
+                Hello
+            </Card>
+            </div>
           </Box>
         
     </>
