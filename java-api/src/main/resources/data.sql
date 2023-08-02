fce5900d34e9cf01cd2a794cdd50c93555ca372d
hammockryan8 <hammockryan@gmail.com>
2023-08-02 12:27:43 -0400

    fixed some column issues

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -1,5 +1,3 @@
-
-
 INSERT INTO bonds (trade_type, trade_currency, quantity, trade_settlement_date, trade_status, trade_date, unit_price, coupon_percent, bond_currency, cusip, face_value, isin, issuer_name, bond_maturity_date, status, type, book_name, bond_holder) VALUES
-('buy','USD',50,	'2021-08-04',	'open',	'2021-05-13',	90,	4.37,	'USD',	1000,	'XS1988387210',	'BNPParibasIssu', 4.37, 'Microsoft Corp (USD)',	'2021-08-05',	'active',	'CORP',	'trading_book_1',	'AZ Holdings Inc');
+('buy', 'USD', 50, '2021-08-04', 'open', '2021-05-13', 90, 4.37, 'USD', NULL, 1000,	'XS1988387210',	'BNPParibasIssu 4,37% Microsoft Corp (USD)', '2021-08-05', 'active', 'CORP', 'trading_book_1', 'AZ Holdings Inc');
 
