815aac398463fd8a88158abcd0a421b57c01c067
Akshay Pujare <akshaypujare7@gmail.com>
2023-08-02 22:11:35 +0100

    added book and security data

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -1,3 +1,13 @@
--- INSERT INTO dogs (dog_id, name, age) VALUES (1, 'Einstein', 3);
--- INSERT INTO dogs (dog_id, name, age) VALUES (2, 'Kaya', 5);
--- INSERT INTO dogs (dog_id, name, age) VALUES (3, 'Lassie', 7);
+-- Book Data
+INSERT INTO book (id,status,type,name) VALUES (1, 'active', 'CORP', 'trading_book_1');
+INSERT INTO book (id,status,type,name) VALUES (2, 'active', 'CORP', 'trading_book_2');
+INSERT INTO book (id,status,type,name) VALUES (3, 'active', 'CORP', 'trading_book_3');
+INSERT INTO book (id,status,type,name) VALUES (4, 'active', 'SOVN', 'trading_book_4');
+INSERT INTO book (id,status,type,name) VALUES (5, 'active', 'GOVN', 'trading_book_6');
+
+--Security Data
+INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, issuer_name, bond_maturity_date) VALUES (1,4.37,'USD','',1000,'XS1988387210','BNPParibasIssu 4,37% Microsoft Corp (USD)','2021-08-05');
+INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, issuer_name, bond_maturity_date) VALUES (2,3.15,'USD','123456780',900,'USN0280EAR64','Airbus 3.15%  USD','2021-07-30');
+INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, issuer_name, bond_maturity_date) VALUES (3,2,'USD','123456bh0',900,'A123456111','UBS Facebook (USD)','2021-09-30');
+INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, issuer_name, bond_maturity_date) VALUES (4,2.02,'USD','87973RAA8',690,'US87973RAA86','TEMASEK FINL I LTD GLOBAL MEDIUM TERM NTS BOOK ENTRY REG S','2021-08-06');
+INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, issuer_name, bond_maturity_date) VALUES (5,0.75,'GBP','BDCHBW8',900,'GB00B6460510','HM Treasury United Kingdon','2021-08-09');
