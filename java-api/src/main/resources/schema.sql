7d59b8a5ebf44a468c02499970f8f8eabdde1f73
hammockryan8 <hammockryan@gmail.com>
2023-08-02 11:19:49 -0400

    updating the SQL schema for bonds

--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -1,8 +1,23 @@
-DROP TABLE IF EXISTS dogs;
+DROP TABLE IF EXISTS bonds;
 
-CREATE TABLE dogs (
-    dog_id INT NOT NULL,
-    name VARCHAR(250) NOT NULL,
-    age INT NOT NULL
+CREATE TABLE bonds (
+    trade_type VARCHAR(50) NOT NULL,
+    trade_currency VARCHAR(50) NOT NULL,
+    quantity INT NOT NULL,
+    trade_settlement_date DATE NOT NULL,
+    trade_status VARCHAR(50),
+    trade_date DATE NOT NULL,
+    unit_price DECIMAL NOT NULL,
+    coupon_percent DECIMAL NOT NULL,
+    bond_currency VARCHAR(50) NOT NULL,
+    cusip VARCHAR(50),
+    face_value INT NOT NULL,
+    isin VARCHAR(50) NOT NULL,
+    issuer_name VARCHAR(50) NOT NULL,
+    bond_maturity_date DATE NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    book_name VARCHAR(50) NOT NULL,
+    bond_holder VARCHAR(50) NOT NULL,
 );
 
