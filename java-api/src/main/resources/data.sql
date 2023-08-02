7bf2ed70896dfb28ea32fe63b40fa15c3318b00d
kpg123 <48265598+kpg123@users.noreply.github.com>
2023-08-02 10:56:31 -0400

    Update data.sql with books

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -1,3 +1,6 @@
-INSERT INTO dogs (dog_id, name, age) VALUES (1, 'Einstein', 3);
-INSERT INTO dogs (dog_id, name, age) VALUES (2, 'Kaya', 5);
-INSERT INTO dogs (dog_id, name, age) VALUES (3, 'Lassie', 7);
+INSERT INTO tableName (book_name) VALUES ('trading_book_1');
+INSERT INTO tableName (book_name) VALUES ('trading_book_2');
+INSERT INTO tableName (book_name) VALUES ('Trading_book_3');
+INSERT INTO tableName (book_name) VALUES ('Trading_book_2');
+INSERT INTO tableName (book_name) VALUES ('Trading_book_4');
+INSERT INTO tableName (book_name) VALUES ('Trading_book_6');
