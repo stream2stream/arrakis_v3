a902f8041b9e35de44bb1b4289682ec1fcce9645
BuildTools <unconfigured@null.spigotmc.org>
2023-08-02 16:09:54 -0400

    Setup Sample Dashboard Component for React Front-End

---
+++ react-app/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import {DataGrid} from '@mui/x-data-grid';
+
+export const Dashboard = () => {
+    const rows = [
+        {id: 1, name: "Brion", favoriteFood: "snowcrab"},
+        {id: 2, name: "Bjorn", favoriteFood: "chicken parmesan"}
+    ];
+    const cols = [
+        {field: "id", headerName: "ID", width: 150},
+        {field: "name", headerName: "Name", width: 150},
+        {field: "favoriteFood", headName: "Food of Favorite", width: 150}
+    ];
+    return (
+        <div>
+            <DataGrid rows={rows} columns={cols}/>
+        </div>
+    )
+}
