bf1c72c400a3b6a1f826867176a5f3730e44a20f
Mark Muthalaly <markgmuthalaly@gmail.com>
2023-08-02 15:44:43 +0100

    added schema structure

--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -1,8 +1,32 @@
 DROP TABLE IF EXISTS dogs;
 
-CREATE TABLE dogs (
-    dog_id INT NOT NULL,
-    name VARCHAR(250) NOT NULL,
-    age INT NOT NULL
+CREATE TABLE security (
+    id int auto_increment PRIMARY KEY, float couponPercent, varchar(3) bondCurrency, varchar(50) cusip, int faceValue,
+    varchar(50) isin, varchar(10) bondsMaturityDate, varchar(10) status, varchar(4) type, varchar(50) issuerName
+
+);
+
+CREATE TABLE users (
+    id int auto_increment PRIMARY KEY, varchar(50) username
+);
+
+CREATE TABLE book (
+    id int auto_increment PRIMARY KEY, varchar(20) bookName
+);
+
+CREATE TABLE book_user (
+    bookID int, FOREIGN KEY (bookID) REFERENCES book(id), int userID, FOREIGN KEY (userID) REFERENCES users(id);
+);
+
+CREATE TABLE counter_party (
+    id int auto_increment PRIMARY KEY, varchar(50) bondHolder
+);
+
+CREATE TABLE trade (
+    id int auto_increment PRIMARY KEY, varchar(4) tradeType, varchar(3) tradeCurrency, int quantity,
+        varchar(10) tradeSettleDate, varchar(10) tradeStatus, varchar(10) tradeDate, float unitPrice,
+        int counterPartyID, FOREIGN KEY (counterPartyID) REFERENCES counter_party(id),
+        int securityID, FOREIGN KEY (securityID) REFERENCES security(id),
+        int bookID, FOREIGN KEY (bookID) REFERENCES book(id)
 );
 
