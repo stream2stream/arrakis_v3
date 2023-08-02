8b11e4a918a77c0dd1008087d748019e21a74298
Akshay Pujare <akshaypujare7@gmail.com>
2023-08-02 22:57:35 +0100

    added some more data in security table

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -10,4 +10,16 @@ INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, i
 INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, issuer_name, bond_maturity_date) VALUES (2,3.15,'USD','123456780',900,'USN0280EAR64','Airbus 3.15%  USD','2021-07-30');
 INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, issuer_name, bond_maturity_date) VALUES (3,2,'USD','123456bh0',900,'A123456111','UBS Facebook (USD)','2021-09-30');
 INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, issuer_name, bond_maturity_date) VALUES (4,2.02,'USD','87973RAA8',690,'US87973RAA86','TEMASEK FINL I LTD GLOBAL MEDIUM TERM NTS BOOK ENTRY REG S','2021-08-06');
-INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, issuer_name, bond_maturity_date) VALUES (5,0.75,'GBP','BDCHBW8',900,'GB00B6460510','HM Treasury United Kingdon','2021-08-09');
+INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, issuer_name, bond_maturity_date) VALUES (5,1.23,'USD','87973RAA8',340,'IE00B29LNP31','First Norway Alpha Kl.IV','2030-12-22');
+INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, issuer_name, bond_maturity_date) VALUES (6,0.75,'GBP','BDCHBW8',900,'GB00B6460505','HM Treasury United Kingdon','2021-08-09');
+INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, issuer_name, bond_maturity_date) VALUES (7,0.75,'GBP','BDCHBW8',900,'GB00B6460506','HM Treasury United Kingdon','2021-08-09');
+INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, issuer_name, bond_maturity_date) VALUES (8,0.75,'GBP','BDCHBW8',900,'GB00B6460507','HM Treasury United Kingdon','2021-08-09');
+INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, issuer_name, bond_maturity_date) VALUES (9,0.75,'GBP','BDCHBW8',900,'GB00B6460508','HM Treasury United Kingdon','2021-08-09');
+INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, issuer_name, bond_maturity_date) VALUES (10,0.75,'GBP','BDCHBW8',900,'GB00B6460509','HM Treasury United Kingdon','2021-08-09');
+INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, issuer_name, bond_maturity_date) VALUES (11,0.75,'GBP','BDCHBW8',900,'GB00B6460510','HM Treasury United Kingdon','2021-08-09');
+INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, issuer_name, bond_maturity_date) VALUES (12,0.75,'GBP','BDCHBW8',900,'GB00B6460511','HM Treasury United Kingdon','2021-08-09');
+INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, issuer_name, bond_maturity_date) VALUES (13,0.75,'GBP','BDCHBW8',900,'GB00B6460512','HM Treasury United Kingdon','2021-08-09');
+INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, issuer_name, bond_maturity_date) VALUES (14,0.75,'GBP','BDCHBW8',900,'GB00B6460513','HM Treasury United Kingdon','2021-08-09');
+INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, issuer_name, bond_maturity_date) VALUES (15,0.75,'GBP','BDCHBW8',900,'GB00B6460514','HM Treasury United Kingdon','2021-08-09');
+INSERT INTO security (id,coupon_percent,bond_currency,cusip, face_value, isin, issuer_name, bond_maturity_date) VALUES (16,0.75,'GBP','BDCHBW8',900,'GB00B6460515','HM Treasury United Kingdon','2021-08-09');
+
