a0e5d1302dc2ea8ca4f12cf9ebe4375623809ff2
kpg123 <48265598+kpg123@users.noreply.github.com>
2023-08-02 10:58:42 -0400

    Update data.sql bond_holder

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -4,3 +4,16 @@ INSERT INTO tableName (book_name) VALUES ('Trading_book_3');
 INSERT INTO tableName (book_name) VALUES ('Trading_book_2');
 INSERT INTO tableName (book_name) VALUES ('Trading_book_4');
 INSERT INTO tableName (book_name) VALUES ('Trading_book_6');
+
+INSERT INTO counter_party (bond_holder) VALUES ('AZ Holdings Inc');
+INSERT INTO counter_party (bond_holder) VALUES ('Acme co');
+INSERT INTO counter_party (bond_holder) VALUES ('Acme Co');
+INSERT INTO counter_party (bond_holder) VALUES ('Sovereign Investments');
+INSERT INTO counter_party (bond_holder) VALUES ('Astra Trading Ltd');
+INSERT INTO counter_party (bond_holder) VALUES ('Muncipal Gov Of Orange County');
+INSERT INTO counter_party (bond_holder) VALUES ('Goldman Sachs');
+INSERT INTO counter_party (bond_holder) VALUES ('UBS');
+INSERT INTO counter_party (bond_holder) VALUES ('Barclays');
+INSERT INTO counter_party (bond_holder) VALUES ('British Telecom');
+INSERT INTO counter_party (bond_holder) VALUES ('Pension Holdings');
+INSERT INTO counter_party (bond_holder) VALUES ('Zurich Pension fund 4');
