fa8f4f60de5d2b0891c156ec80b25ebe3908b138
hburn7 <hburnett777@gmail.com>
2023-08-02 11:08:42 -0400

    Adjust typing on BondsRepository

--- java-api/src/main/java/com/db/grad/javaapi/repository/BondsRepository.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/BondsRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 @Repository
 public interface BondsRepository extends JpaRepository<BondsData, Integer>
 {
-    @Query(nativeQuery = true, value = "select * from bondsData where tradeType = :tradeType")
-    List<BondsData> findByName(BondsData tradeType);
+    @Query(nativeQuery = true, value = "select * from bondsData where trade_type = :tradeType")
+    List<BondsData> findByName(String tradeType);
 }
