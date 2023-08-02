20ae50bd6848e25c2136c4a214e438bea06fb19b
Mario <marioirimia@gmail.com>
2023-08-02 14:27:46 +0300

    ZoomBondTable second page

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Home from "./pages/Home";
+import ZoomBond from "./pages/ZoomBond";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigate replace to="/home" />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/zoombond" element={<ZoomBond />} /> 
     </Routes>
   </>);
 };
---
+++ react-app/src/pages/ZoomBond.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ZoomBondTable from "../components/ZoomBondTable";
+
+const ZoomBond = () => {
+  return (
+    <div>
+      <div>This is the ZoomBond page.</div>
+      <ZoomBondTable /> {/* Render the ZoomBondTable component */}
+    </div>
+  );
+};
+
+export default ZoomBond;
