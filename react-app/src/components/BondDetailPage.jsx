e94fc1c6dcf910dd65dc2423ccdcb025617ffb32
zeineb-ouerghi <zeineb@uni.minerva.edu>
2023-08-03 14:14:06 -0400

    tables

--- react-app/src/components/BondDetailPage.jsx
+++ react-app/src/components/BondDetailPage.jsx
@@ -4,8 +4,52 @@ import BondCard from './BondCard'
 const BondDetailPage = (props) => {
   return (<>
         <BondCard bonds= {["test", "test2"]}/>
+      
+      <table style = {{border: '1px solid grey'}}> 
+        <tr>  
+          <th> ISIN </th>
+          <th> Type </th>
+          <th> Issuer </th>
+          <th> Maturity </th>
+          <th> Face Value </th>
+          <th> Currency </th>
+          <th> Coupon % </th>
+          <th> Status</th>
+
+        </tr> 
+        {props.data.map(bonds => {<tr> bonds.map(bond => 
+          {
+          <td> {bond["ISIN "]}</td>
+          <td> {bond["Type"]} </td>
+          <td> {bond["Issuer"]} </td>
+          <td> {bond["Maturity"]} </td>
+          <td> {bond["Face Value"]} </td>
+          <td> {bond["Currency" ]}</td>
+          <td> {bond["Coupon %" ]}</td>
+          <td> {bond["Status"]}</td>
+          })
+          </tr>})
+          }
+      </table>
+
+      <table style = {{border: '1px solid grey'}}> 
+        <tr>  
+          <th> ISIN </th>
+          <th> Book ID </th>
+          <th> Client </th>
+          <th> Status </th>
+          <th> Quantity </th>
+          <th> Buy/Sell </th>
+          <th> Trade Date </th> 
+          <th> Settlement Date</th>
+
+        </tr>
+      </table>
+
+
       </>
   )
 }
+
 
 export default BondDetailPage;
