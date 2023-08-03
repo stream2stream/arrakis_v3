b7035fbb5fd58125477a6db793bbb30cc007566d
Joe Klug <josephmklug@gmail.com>
2023-08-03 21:06:37 +0100

    make routing more robust

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -32,37 +32,43 @@ function App() {
   })
 
   return (
-      <>
-        <Navbar expand="lg" className="bg-body-tertiary">
-          <Container>
-            <Navbar.Brand href="/">Home</Navbar.Brand>
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ml-auto">
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="/">Home</Navbar.Brand>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
 
-                {authenticated &&
-                  <>
-                    <Nav.Link href="/logout">Logout</Nav.Link>
-                    <Nav.Link href="/allbonds">All Bonds</Nav.Link>
-                    <Nav.Link href="/bondsbymaturity">Bonds by Maturity</Nav.Link>
-                  </>}
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+              {authenticated &&
+                <>
+                  <Nav.Link href="/logout">Logout</Nav.Link>
+                  <Nav.Link href="/allbonds">All Bonds</Nav.Link>
+                  <Nav.Link href="/bondsbymaturity">Bonds by Maturity</Nav.Link>
+                </>}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-        <Container className="mt-3">
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={authenticated ? <AllBonds /> : <Login/>} />
-            <Route path="notauthorized" element={<NotAuthorized />} />
-            <Route path="logout" element={<Logout />} />
-            {!isLoading && authenticated && <>
+      <Container className="mt-3">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={authenticated ? <AllBonds /> : <Login />} />
+          <Route path="notauthorized" element={<NotAuthorized />} />
+          <Route path="logout" element={<Logout />} />
+          {!isLoading && authenticated && <>
             <Route path="/allbonds" element={<AllBonds />} />
             <Route path="/bondsbymaturity" element={<AllMaturingBonds />} />
-            </>}
-          </Routes>
-        </Container>
-      </>
+            <Route path="/*" element={<AllBonds />} />
+          </>}
+          {!isLoading && !authenticated && <>
+            <Route path="/*" element={<Login />} />
+          </>}
+          {isLoading &&
+            <Route path="/*" element={<div />} />}
+        </Routes>
+      </Container>
+    </>
   );
 }
 
