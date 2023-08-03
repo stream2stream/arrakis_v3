6edfaac679946ea66996d650f0aeb948b81e9538
BadulescuCatalin <badulescucatalin01@gmail.com>
2023-08-03 11:03:28 +0300

    "not finished"

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -5,3 +5,54 @@ INSERT INTO dogs (dog_id, name, age) VALUES (3, 'Lassie', 7);
 INSERT INTO bonds (id, bond_maturity_date, isin) VALUES (1, '01/08/2023', '1234ABCD');
 INSERT INTO bonds (id, bond_maturity_date, isin) VALUES (2, '05/08/2023', '1234ABCE');
 INSERT INTO bonds (id, bond_maturity_date, isin) VALUES (3, '10/08/2023', '1234ABCF');
+
+INSERT INTO users (name, email, password) VALUES ('Badulescu Catalin', 'catalin@gmail.com', 'abcd');
+INSERT INTO users (name, email, password) VALUES ('Petrovici Vlad', 'vlad@gmail.com', '1234');
+
+INSERT INTO books (book_name)
+SELECT DISTINCT book_name FROM CSVREAD('.\\java-api\\src\\main\\resources\\db-bonds-data.csv');
+
+--INSERT INTO security (security_id, isin, bond_name, cusip,bond_type,bond_status,bond_maturity_date,bond_currency,issuer_name,trade_id )
+--VALUES (1,'XS1988387210','AZ Holdings Inc', ,'CORP','active','05-08-2021','USD','BNPParibasIssu 4,37% Microsoft Corp (USD)',{1,2}),
+--VALUES (2,'USN0280EAR64','Acme co', '123456780','CORP','active','30-07-2021','USD','Airbus 3.15%  USD',{3,4}),
+--
+--VALUES (2,'USN0280EAR64','Astra Trading Ltd','123456780','CORP','active','30-07-2021','USD','Airbus 3.15%  USD',6),
+--VALUES (3,'A12356111','Sovereign Investments', '123456bh0','CORP','active','30-09-2021','USD','UBS Facebook (USD)',5),
+--VALUES (3,'A12356111','Sovereign Investments', '123456bh0','CORP','active','30-09-2021','USD','UBS Facebook (USD)',7),
+--VALUES (4,'USU02320AG12','Muncipal Gov Of Orange County', 'AMZN 3.15 08/22/27 REGS','CORP','active','30-08-2021','USD','Amazon',8),
+--VALUES (4,'USU02320AG12','Muncipal Gov Of Orange County', 'AMZN 3.15 08/22/27 REGS','CORP','active','30-08-2021','USD','Amazon',9),
+--VALUES (5,'B00B6460505','Goldman Sachs', 'BDCHBW8','GOVN','active','09-08-2021','GBP','HM Treasury United Kingdon',10),
+--VALUES (6,'B00B6460506','Goldman Sachs', 'BDCHBW8',,'GOVN','active','09-08-2021','GBP','HM Treasury United Kingdon',11),
+--VALUES (7,'B00B6460507','UBS', 'BDCHBW8','GOVN','active','09-08-2021','GBP','HM Treasury United Kingdon',12),
+--VALUES (8,'B00B6460508','UBS', 'BDCHBW8','GOVN','active','09-08-2021','GBP','HM Treasury United Kingdon',13),
+--VALUES (9,'B00B6460509','Barclays', 'BDCHBW8','GOVN','active','09-08-2021','GBP','HM Treasury United Kingdon',14),
+--VALUES (10,'B00B6460510','Barclays', 'BDCHBW8','GOVN','active','09-08-2021','GBP','HM Treasury United Kingdon',15),
+--VALUES (11,'B00B6460511','Barclays', 'BDCHBW8','GOVN','active','09-08-2021','GBP','HM Treasury United Kingdon',16),
+--VALUES (12,'B00B6460512','British Telecom', 'BDCHBW8','GOVN','active','09-08-2021','GBP','HM Treasury United Kingdon',17),
+--VALUES (13,'B00B6460513','Pension Holdings', 'BDCHBW8','GOVN','active','09-08-2021','GBP','HM Treasury United Kingdon',18),
+--VALUES (14,'B00B6460514','Pension Holdings', 'BDCHBW8','GOVN','active','09-08-2021','GBP','HM Treasury United Kingdon',19),
+--VALUES (15,'B00B6460515','Pension Holdings', 'BDCHBW8','GOVN','active','09-08-2021','GBP','HM Treasury United Kingdon',20),
+--VALUES (16,'Zurich Pension fund 4', '87973RAA8','US87973RAA86','SOVN','active','06-08-2021','USD'),
+--VALUES ('Zurich Pension fund 4', '87973RAA8','IE00B29LNP31','SOVN','active','22-12-2030','USD');
+--
+--INSERT INTO books (book_name) VALUES ('trading_book_1');
+--INSERT INTO books (book_name) VALUES ('trading_book_2');
+--INSERT INTO books (book_name) VALUES ('trading_book_2');
+--INSERT INTO books (book_name) VALUES ('trading_book_3');
+--INSERT INTO books (book_name) VALUES ('trading_book_4');
+--INSERT INTO books (book_name) VALUES ('trading_book_6');
+--
+--INSERT INTO Counterparty(name) VALUES ('AZ Holdings Inc');
+--INSERT INTO Counterparty(name) VALUES ('Acme co');
+--INSERT INTO Counterparty(name) VALUES ('Sovereign Investments');
+--INSERT INTO Counterparty(name) VALUES ('Astra Trading Ltd');
+--INSERT INTO Counterparty(name) VALUES ('Muncipal Gov Of Orange County');
+--INSERT INTO Counterparty(name) VALUES ('Goldman Sachs');
+--INSERT INTO Counterparty(name) VALUES ('UBS');
+--INSERT INTO Counterparty(name) VALUES ('Barclays');
+--INSERT INTO Counterparty(name) VALUES ('British Telecom');
+--INSERT INTO Counterparty(name) VALUES ('Pension Holdings');
+--INSERT INTO Counterparty(name) VALUES ('Zurich Pension fund 4');
+
+--INSERT INTO trade (trade_id, trade_type,trade_currency,trade_currency,trade_quantity,trade_settlement_date,trade_status,trade_date,trade_unit_price)
+--VALUES ('AZ Holdings Inc', ,'XS1988387210','CORP','active','05-08-2021','USD'),
--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -1,10 +1,71 @@
 DROP TABLE IF EXISTS dogs;
+DROP TABLE IF EXISTS trades;
+DROP TABLE IF EXISTS book_user;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS security;
+DROP TABLE IF EXISTS counter_party;
 DROP TABLE IF EXISTS bonds;
 
 CREATE TABLE dogs (
     dog_id INT NOT NULL,
     name VARCHAR(250) NOT NULL,
     age INT NOT NULL
+);
+
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(250) NOT NULL,
+    email VARCHAR(250) NOT NULL,
+    password VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE books (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    book_name VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE book_user (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    book_id INT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (book_id) REFERENCES books(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE security (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    isin VARCHAR(50) DEFAULT NULL,
+    cusip VARCHAR(50) DEFAULT NULL,
+    issuer_name VARCHAR(250) NOT NULL,
+    maturity_date VARCHAR(50) NOT NULL,
+    coupon FLOAT NOT NULL,
+    type VARCHAR(250) NOT NULL,
+    face_value FLOAT NOT NULL,
+    currency VARCHAR(5) NOT NULL,
+    status VARCHAR(50) DEFAULT NULL
+);
+
+CREATE TABLE counter_party (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE trades (
+    id INT PRIMARY KEY AUTO_INCREMENT
+    book_id INT NOT NULL,
+    security_id INT NOT NULL,
+    counter_party_id INT NOT NULL,
+    currency VARCHAR(5) NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    quantity INT NOT NULL,
+    unit_price FLOAT NOT NULL,
+    buy_sell VARCHAR(50) NOT NULL,
+    trade_date VARCHAR(50) NOT NULL,
+    settlement_date VARCHAR(50) NOT NULL,
+    FOREIGN KEY (book_id) REFERENCES books(id),
+    FOREIGN KEY (security_id) REFERENCES security(id),
+    FOREIGN KEY (counter_party_id) REFERENCES counter_party(id)
 );
 
 CREATE TABLE bonds (
