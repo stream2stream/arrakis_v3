f10e9fcdbe6c31cb934de426a31367fd6e526ecb
hburn7 <hburnett777@gmail.com>
2023-08-03 10:09:44 -0400

    Added NotAuthorized component

---
+++ react-app/src/components/pets/NotAuthorized.jsx
@@ -0,0 +1,10 @@
+const NotAuthorized = () => {
+    return (
+        <div className="container">
+        <h1>Not Authorized</h1>
+        <p>You are not authorized to view this page.</p>
+        </div>
+    );
+}
+
+export default NotAuthorized;
