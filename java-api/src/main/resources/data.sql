ab28b9729323223e95135c5ad3fa15d10f8bfa65
unknown <mtalturki42@tntech.edu>
2023-08-03 16:57:38 +0100

    modified schema and data sql files

--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -1,11 +1,2 @@
-INSERT INTO dogs (dog_id, name, age) VALUES (1, 'Einstein', 3);
-INSERT INTO dogs (dog_id, name, age) VALUES (2, 'Kaya', 5);
-INSERT INTO dogs (dog_id, name, age) VALUES (3, 'Lassie', 7);
+INSERT INTO book (id, name) VALUES (1, 'Einstein');
 
-BULK INSERT bonds
-FROM 'C:/Users/Admin/Downloads/db-bonds-data.csv'
-WITH (
-    FIELDTERMINATOR = ',',
-    ROWTERMINATOR = '\n',
-    FIRSTROW = 2
-);
--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -1,36 +1,33 @@
-DROP DATABASE IF EXISTS `bonds`;
-CREATE DATABASE `bonds`;
-use bonds;
+DROP ALL OBJECTS;
 
-CREATE TABLE `book` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `role` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE book (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name varchar(255) NOT NULL
+);
 
-CREATE TABLE `counterparty` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE userr (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  role varchar(255) NOT NULL
+);
 
-CREATE TABLE `book_user` (
+CREATE TABLE counterparty (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE book_user (
   `book_id` int NOT NULL,
   `user_id` int NOT NULL,
-  KEY `FK1_book_id` (`book_id`),
-  KEY `FK_user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  FOREIGN KEY (`book_id`) REFERENCES book(id),
+  FOREIGN KEY (`user_id`) REFERENCES userr(id)
+);
 
 
-CREATE TABLE `security` (
+CREATE TABLE security (
   `id` int NOT NULL AUTO_INCREMENT,
   `isin` varchar(50) DEFAULT NULL,
   `cusip` varchar(50) DEFAULT NULL,
@@ -42,9 +39,9 @@ CREATE TABLE `security` (
   `currency` varchar(10) NOT NULL,
   `status` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
-CREATE TABLE `trades` (
+CREATE TABLE trades (
   `id` int NOT NULL AUTO_INCREMENT,
   `book_id` int NOT NULL,
   `security_id` int NOT NULL,
@@ -57,10 +54,10 @@ CREATE TABLE `trades` (
   `trade_date` datetime NOT NULL,
   `settlement_date` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK_security_id` (`security_id`),
-  KEY `FK_counterparty_id` (`counterparty_id`),
-  KEY `FK_book_id` (`book_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  FOREIGN KEY (`security_id`) REFERENCES security(id),
+  FOREIGN KEY (`counterparty_id`) REFERENCES counterparty(id),
+  FOREIGN KEY (`book_id`) REFERENCES book(id)
+);
 
 
 ALTER TABLE book_user ADD PRIMARY KEY(book_id, user_id);
