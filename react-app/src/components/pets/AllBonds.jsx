38498bf9f752d7930373d65e7b0ed89b6ebcf3d6
Thin YatiSu <thinyatihsu.hsu@gmail.com>
2023-08-03 18:15:29 +0100

    having all bonds display the table instead of bond detail

--- react-app/src/components/pets/AllBonds.jsx
+++ react-app/src/components/pets/AllBonds.jsx
@@ -9,7 +9,24 @@ const AllBonds = () => {
     // bonds.map(bond =>(
     //     <BondDetail info={bond} key={bond.isin.toString()} />
     // ))
-    <BondDetail/>
+    <table>
+      <thead>
+        <th>ISIN</th>
+        <th>Face Value</th>
+        <th>Coupon %</th>
+      </thead>
+      <tbody>
+        {bonds.map((row, index) => (
+          <tr key={index}>
+            <td>{row.isin}</td>
+            <td>{row.faceValue}</td>
+            <td>{row.faceValue}</td>
+            <td>{row.coupon_percent}</td>
+          </tr>
+        ))}
+
+      </tbody>
+    </table>
   )
 }
 
--- react-app/src/components/pets/BondDetail.jsx
+++ react-app/src/components/pets/BondDetail.jsx
@@ -3,26 +3,9 @@ import { getAllBonds } from '../../services/BondServices'
 import '../../App.css'
 
 const BondDetail = (props) => {
-  var bonds = getAllBonds();
   return (
-    <table>
-      <thead>
-        <th>ISIN</th>
-        <th>Face Value</th>
-        <th>Coupon %</th>
-      </thead>
-      <tbody>
-        {bonds.map((row, index) => (
-          <tr key={index}>
-            <td>{row.isin}</td>
-            <td>{row.faceValue}</td>
-            <td>{row.coupon_percent}</td>
-          </tr>
-        ))}
-
-      </tbody>
-    </table>
-
+    <></>
+  
     // <div>
     //     <h1>{props.info.issuerName}</h1>
     //     <p>Face Value: {props.info.faceValue} &emsp; Coupon: {props.info.couponPercent} &emsp; Maturity Date: {props.info.bondMaturityDate} &emsp; ISIN: {props.info.isin}</p>
