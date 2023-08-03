487b16c3a84950b7aeb0fd6204e4f0fed44a80b6
Mark Muthalaly <markgmuthalaly@gmail.com>
2023-08-03 19:58:32 +0100

    added UsersRepository

---
+++ java-api/src/main/java/com/db/grad/javaapi/repository/UsersRepository.java
@@ -0,0 +1,9 @@
+package com.db.grad.javaapi.repository;
+
+import com.db.grad.javaapi.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository {
+    User getUserByUsername(String username);
+    User findById(long id);
+}
