e3a1e4a4a89fe7811a136fd59ec37a68e8efe73a
hburn7 <hburnett777@gmail.com>
2023-08-03 11:30:53 -0400

    Remove unused navbar toggle

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -15,7 +15,6 @@ function App() {
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
           <Navbar.Brand href="/">Home</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="/allbonds">All Bonds</Nav.Link>
