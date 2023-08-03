38a5976c198732c7fd6e32c758555643e87bdb9a
Daniel <djhenderson00@gmail.com>
2023-08-03 18:24:41 +0100

    Ability to switch between pages

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,14 +2,25 @@ import React from "react";
 import { Pets } from "./components/pets/Pets";
 import { HomePage } from "./components/HomePage";
 import NavigationBar from "./components/NavigationBar";
+import { useState } from "react";
 
 const App = () => {
-  return (
-    <>
-      <NavigationBar />
-      <HomePage />
-    </>
-  )
+  const [selectedTab, setSelectedTab] = useState(0);
+  if (selectedTab === 0) {
+    return (
+      <>
+        <NavigationBar setTab={setSelectedTab} />
+        <HomePage />
+      </>
+    )
+  } else {
+    return (
+      <>
+        <NavigationBar setTab={setSelectedTab} />
+        <Pets />
+      </>
+    )
+  }
 };
 
 export default App;
--- react-app/src/components/NavigationBar.jsx
+++ react-app/src/components/NavigationBar.jsx
@@ -1,25 +1,19 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
 import { Tab, Tabs } from '@mui/material';
 import { useState } from 'react';
-// import {CustomTabPanel} from '@mui/material';
 
 const pages = ['Home', 'Books', 'Upcoming'];
 
-function handleBarClick(params) {
-    console.log(params);
-  }
-
-function NavigationBar() {
+const NavigationBar = (props) => {
 
     const [selectedTab, setSelectedTab] = useState(0);
 
     const handleTabChange = (event, newValue) => {
         setSelectedTab(newValue);
+        props.setTab(newValue);
         console.log(newValue);
     };
 
@@ -29,10 +23,10 @@ function NavigationBar() {
 
   return (
     <>
-    <AppBar position="static"  sx={{ 'background-color': '#627d97'}}>
+    <AppBar position="static"  sx={{ 'background-color': '#e8f6ff'}}>
         <Container maxWidth="xl">
             <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', 'justify-content': 'space-around' } }}>
-                <Tabs value={selectedTab} onChange={handleTabChange}>
+                <Tabs value={selectedTab} onChange={handleTabChange} sx={{color: 'white'}}>
                     <Tab label={pages[0]} />
                     <Tab label={pages[1]}  />
                     <Tab label={pages[2]}  />
