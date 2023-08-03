64c7967a63eaf3ca396162a3a0c8453aef6398c4
Daniel Oh <danieloh01@yahoo.com>
2023-08-03 10:48:41 -0400

    added basic maturity table

--- react-app/src/services/TradeServices.js
+++ react-app/src/services/TradeServices.js
@@ -6,6 +6,9 @@ import http from './axios-common';
 export function getAllBonds(){
   return http.get("/bonds");
 }
+export function getAllBondsMaturity(){
+  return http.get("/bonds/maturity");
+}
 
 // export const findBonds = () => {
 //   const pets = axios.get(`${hostNameUrl}/dogs`);
