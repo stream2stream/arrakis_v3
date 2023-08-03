215f83a1ad286afe6db4237d30eb74a55318905a
ethanzeh <ethanzeh21@gmail.com>
2023-08-03 10:10:08 +0200

    deleted some dog files, deleted unnecessary bookUser models

--- java-api/src/main/java/com/db/grad/javaapi/repository/BookUserRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.db.grad.javaapi.repository;
-//
-//import com.db.grad.javaapi.model.BookUser;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface BookUserRepository extends JpaRepository<BookUser, Long> {
-//}
