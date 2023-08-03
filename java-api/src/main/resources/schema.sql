1d77a7c99b052f16fbce2394c64fb37639f0f99e
deepika57 <amruthadeepika@gmail.com>
2023-08-03 14:02:58 +0100

    Changes in database file

--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS dogs;
+//DROP TABLE IF EXISTS dogs;
 DROP TABLE IF EXISTS trades;
 DROP TABLE IF EXISTS book_user;
 DROP TABLE IF EXISTS users;
@@ -7,17 +7,18 @@ DROP TABLE IF EXISTS security;
 DROP TABLE IF EXISTS counter_party;
 DROP TABLE IF EXISTS bonds;
 
-CREATE TABLE dogs (
-    dog_id INT NOT NULL,
-    name VARCHAR(250) NOT NULL,
-    age INT NOT NULL
-);
+--CREATE TABLE dogs (
+--    dog_id INT NOT NULL,
+--    name VARCHAR(250) NOT NULL,
+--    age INT NOT NULL
+--);
 
 CREATE TABLE users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT NOT NULL PRIMARY KEY  AUTO_INCREMENT,
     name VARCHAR(250) NOT NULL,
     email VARCHAR(250) NOT NULL,
-    password VARCHAR(250) NOT NULL
+    password VARCHAR(250) NOT NULL,
+    role VARCHAR(250) NOT NULL
 );
 
 CREATE TABLE books (
@@ -68,20 +69,20 @@ CREATE TABLE trades (
     FOREIGN KEY (counter_party_id) REFERENCES counter_party(id)
 );
 
-CREATE TABLE bonds (
-    id INT NOT NULL,
-    bond_maturity_date VARCHAR(250) NOT NULL,
-    isin VARCHAR(250) NOT NULL
-);
-
-CREATE VIEW IF NOT EXISTS all_bonds_old as
-SELECT  id, bond_maturity_date, isin from bonds;
-
-CREATE VIEW IF NOT EXISTS all_bonds as
-SELECT  s.isin, s.cusip, s.issuer_name, s.maturity_date as bond_maturity_date, s.coupon, s.type as bond_type , s.face_value,
-    s.currency as bond_currency, s.status as bond_status,
-    t.currency as trade_currency, t.status as trade_status, t.quantity, t.unit_price, t.buy_sell, t.trade_date,
-    t.settlement_date, b.book_name, c.name as book_holder
-FROM security s join trades t on s.id = t.security_id join books b on t.book_id = b.id join counter_party c on t.counter_party_id = c.id;
-
+--CREATE TABLE bonds (
+--    id INT NOT NULL,
+--    bond_maturity_date VARCHAR(250) NOT NULL,
+--    isin VARCHAR(250) NOT NULL
+--);
+--
+--CREATE VIEW IF NOT EXISTS all_bonds_old as
+--SELECT  id, bond_maturity_date, isin from bonds;
+--
+--CREATE VIEW IF NOT EXISTS all_bonds as
+--SELECT  s.isin, s.cusip, s.issuer_name, s.maturity_date as bond_maturity_date, s.coupon, s.type as bond_type , s.face_value,
+--    s.currency as bond_currency, s.status as bond_status,
+--    t.currency as trade_currency, t.status as trade_status, t.quantity, t.unit_price, t.buy_sell, t.trade_date,
+--    t.settlement_date, b.book_name, c.name as book_holder
+--FROM security s join trades t on s.id = t.security_id join books b on t.book_id = b.id join counter_party c on t.counter_party_id = c.id;
+--
 
