41a81cd48e854d5d14d84a3c64c8df62c25b7a3b
zeineb-ouerghi <zeineb@uni.minerva.edu>
2023-08-03 14:45:14 -0400

    few changes

--- react-app/src/components/BondDetailPage.jsx
+++ react-app/src/components/BondDetailPage.jsx
@@ -3,7 +3,8 @@ import BondCard from './BondCard'
 
 const BondDetailPage = (props) => {
   let data = [{ISIN : 'A12356112', Type : 'Sovereign' , Issuer :'BunderBank', Maturity : '15/07/2021'
-, FaceValue: '900', Currency: 'USD', Coupon : '2', Status: 'active', BookID:'book4', Client: 'Goldman Sachs', Quantity: '60', UnitPrice: '70.5', Currency: "GBP", Buysell: 'buy', TradeDate: '04/02/2021', SettlementDate: '27/09/2021' }]
+, FaceValue: '900', BondCurrency: 'USD', Coupon : '2', Status: 'active', BookID:'book4', Client: 'Goldman Sachs', Quantity: '60', UnitPrice: '70.5',  
+TradeCurrency: "GBP", Buysell: 'buy', TradeDate: '04/02/2021', SettlementDate: '27/09/2021' , TradeStatus: 'open'}]
 
   return (<>
         <BondCard bonds= {["test", "test2"]}/>
@@ -15,7 +16,7 @@ const BondDetailPage = (props) => {
           <th> Issuer </th>
           <th> Maturity </th>
           <th> Face Value </th>
-          <th> Currency </th>
+          <th> Bond Currency </th>
           <th> Coupon % </th>
           <th> Status</th>
 
@@ -27,7 +28,7 @@ const BondDetailPage = (props) => {
                   <td>{bond["Issuer"]}</td>
                   <td>{bond["Maturity"]}</td>
                   <td>{bond["FaceValue"]}</td>
-                  <td>{bond["Currency"]}</td>
+                  <td>{bond["BondCurrency"]}</td>
                   <td>{bond["Coupon"]}</td>
                   <td>{bond["Status"]}</td>
               </tr>
@@ -39,8 +40,10 @@ const BondDetailPage = (props) => {
           <th> ISIN </th>
           <th> Book ID </th>
           <th> Client </th>
-          <th> Status </th>
+          <th> Trade Status </th>
           <th> Quantity </th>
+          <th> Unit Price </th>
+          <th> Trade Currency </th>  
           <th> Buy/Sell </th>
           <th> Trade Date </th> 
           <th> Settlement Date</th>
@@ -51,8 +54,10 @@ const BondDetailPage = (props) => {
                   <td>{bond["ISIN"]}</td>
                   <td>{bond["BookID"]}</td>
                   <td>{bond["Client"]}</td>
-                  <td>{bond["Status"]}</td>
+                  <td>{bond["TradeStatus"]}</td>
                   <td>{bond["Quantity"]}</td>
+                  <td>{bond["UnitPrice"]}</td>
+                  <td>{bond["TradeCurrency"]}</td>
                   <td>{bond["Buysell"]}</td>
                   <td>{bond["TradeDate"]}</td>
                   <td>{bond["SettlementDate"]}</td>
