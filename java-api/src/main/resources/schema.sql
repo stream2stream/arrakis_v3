053ad70fbfa98d2acf71ed1ef746478b2f560fec
Benjamin Linford <blinford@live.unc.edu>
2023-08-03 09:36:37 -0400

    adjusted schema sql

--- java-api/src/main/resources/schema.sql
+++ java-api/src/main/resources/schema.sql
@@ -1,56 +1,26 @@
--- DROP TABLE IF EXISTS dogs;
---
--- CREATE TABLE dogs (
---     dog_id INT NOT NULL,
---     name VARCHAR(250) NOT NULL,
---     age INT NOT NULL
--- );
-DROP TABLE IF EXISTS bonds;
+-- CREATE TABLE `bonds` (
+--     `id` int NOT NULL AUTO_INCREMENT,
+--     `isin` varchar(50) DEFAULT NULL,
+--     `cusip` varchar(50) DEFAULT NULL,
+--     `issuer_name` varchar(255) NOT NULL,
+--     `bond_maturity` date NOT NULL,
+--     `coupon_percent` float NOT NULL,
+--     `type` varchar(255) NOT NULL,
+--     `face_value` float NOT NULL,
+--     `bond_currency` varchar(10) NOT NULL,
+--     `status` varchar(32) DEFAULT NULL,
+--     PRIMARY KEY (`id`)
+-- )
+DROP TABLE bonds IF EXISTS;
 
 CREATE TABLE bonds (
-    coupon_percent float NOT NULL,
-    bond_currency varchar NOT NULL,
-    cusip varchar,
+    isin varchar(50) PRIMARY KEY,
+    cusip varchar(50),
+    bond_currency varchar(10) NOT NULL,
     face_value int NOT NULL,
-    isin varchar PRIMARY KEY,
-    bond_maturity varchar NOT NULL,
-    issuer_name varchar NOT NULL,
-    status varchar NOT NULL,
-    type varchar NOT NULL
+    bond_maturity varchar(10) NOT NULL,
+    coupon_percent float NOT NULL,
+    issuer_name varchar(255) NOT NULL,
+    status varchar(32) NOT NULL,
+    type varchar(255) NOT NULL
 );
-
--- DROP TABLE IF EXISTS books;
---
--- CREATE TABLE books (
---     name varchar PRIMARY KEY
--- );
---
--- DROP TABLE IF EXISTS users;
---
--- CREATE TABLE users ( // who is the user? ask Sel
---     id int PRIMARY KEY
--- );
---
--- DROP TABLE IF EXISTS counterparties;
---
--- CREATE TABLE counterparties (
---     id int PRIMARY KEY AUTO_INCREMENT,
---     name varchar NOT NULL,
--- );
---
--- DROP TABLE IF EXISTS trades;
---
--- CREATE TABLE trades (
---     id int PRIMARY KEY AUTO_INCREMENT,
---     trade_type varchar NOT NULL,
---     trade_currency varchar NOT NULL,
---     quantity int NOT NULL,
---     trade_status varchar NOT NULL,
---     trade_date date NOT NULL,
---     isin int NOT NULL, // foreign key
---     book_name varchar NOT NULL, // foreign key
---     bond_holder varchar NOT NULL, // foreign key
---     FOREIGN key (isin) REFERENCES bonds (isin),
---     FOREIGN key (book_name) REFERENCES books (name),
---     FOREIGN key (bond_holder) REFERENCES counterparties (name),
--- );
