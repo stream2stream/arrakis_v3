1f8b62e4f04e8d204e0f530d5321cea829a91eb4
hammockryan8 <hammockryan8@gmail.com>
2023-08-03 16:31:10 +0100

    creating a user object

---
+++ java-api/src/main/java/com/db/grad/javaapi/model/User.java
@@ -0,0 +1,15 @@
+package com.db.grad.javaapi.model;
+
+public class User {
+
+    private String name;
+    private String email;
+    private String role;
+
+    public User(String name, String email, String role){
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+
+}
