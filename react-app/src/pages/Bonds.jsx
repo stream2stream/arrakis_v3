9f66c816292c48a2a25b67fe3b7bb286b3d437f6
Dishu Bagga <dishubagga9111@gmail.com>
2023-08-03 11:15:44 +0200

    type of data

--- react-app/src/pages/Bonds.jsx
+++ react-app/src/pages/Bonds.jsx
@@ -6,11 +6,16 @@ import Row from "react-bootstrap/Row";
 import Typography from '@mui/material/Typography';
 import BondCard from '../components/BondCard';
 
+const data = [
+    {"date": "03/08/2023", "typeOfBondsAndMaturedOnThisDay": [{'typeOfBond': 'GOVN', 'NumberOfBondMatured': 2}, {'typeOfBond': 'CORP', 'NumberOfBondMatured': 3}]},
+    {"date": "03/08/2023", "typeOfBondsAndMaturedOnThisDay": [{'typeOfBond': 'GOVN', 'NumberOfBondMatured': 2}, {'typeOfBond': 'CORP', 'NumberOfBondMatured': 3}]}
+]
+
 function Bonds() {
   return (
     <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <Toolbar />
-       
+       data.map
         <Row className="row">
             <div className="container">
                 <BondCard className="container"/>
