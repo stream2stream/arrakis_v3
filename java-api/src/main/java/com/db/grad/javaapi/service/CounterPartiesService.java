1fd89b459a70a5664a4ede56c24d8733061fa91a
BuildTools <unconfigured@null.spigotmc.org>
2023-08-03 11:48:25 -0400

    Worked on end-points for Book and Counter Party Class.

--- java-api/src/main/java/com/db/grad/javaapi/service/CounterPartiesService.java
+++ java-api/src/main/java/com/db/grad/javaapi/service/CounterPartiesService.java
@@ -1,4 +1,26 @@
 package com.db.grad.javaapi.service;
 
-public interface CounterPartiesService {
+import com.db.grad.javaapi.model.Book;
+import com.db.grad.javaapi.model.CounterParty;
+import com.db.grad.javaapi.repository.CounterPartiesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CounterPartiesService {
+
+    @Autowired
+    private CounterPartiesRepository counterPartiesRepository;
+
+    public List<CounterParty> getAllCounterParties() {
+        return counterPartiesRepository.findAll();
+    }
+
+    public CounterParty getCounterPartyById(int id) {
+        Optional<CounterParty> counterParty = counterPartiesRepository.findById(id);
+        return counterParty.orElse(null);
+    }
 }
