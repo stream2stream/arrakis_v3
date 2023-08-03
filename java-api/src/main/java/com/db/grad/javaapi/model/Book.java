861e8409c50a11b2a925f2d815fab87a64fbcb40
eliooreuxx <luhaida13172@gmail.com>
2023-08-03 10:12:06 +0200

    book entity

---
+++ java-api/src/main/java/com/db/grad/javaapi/model/Book.java
@@ -0,0 +1,40 @@
+package com.db.grad.javaapi.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "book")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(name = "name", nullable = false)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
