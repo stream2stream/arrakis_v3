793a78478d0aad41410b3a9740c73cab29442d40
Pulkit <pulkitkhera97@gmail.com>
2023-08-03 11:25:22 +0100

    Added password hashing logic

--- java-api/src/main/java/com/db/grad/javaapi/repository/UserRepository.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/UserRepository.java
@@ -2,6 +2,11 @@ package com.db.grad.javaapi.repository;
 
 import com.db.grad.javaapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query(nativeQuery = true, value = "select * from users where mail = :mail")
+    List<User> findUserForLogin(String mail);
 }
--- java-api/src/main/java/com/db/grad/javaapi/service/UserService.java
+++ java-api/src/main/java/com/db/grad/javaapi/service/UserService.java
@@ -1,7 +1,45 @@
 package com.db.grad.javaapi.service;
 
+import com.db.grad.javaapi.model.User;
+import com.db.grad.javaapi.repository.UserRepository;
+import com.google.common.hash.Hashing;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.List;
 
 @Service
 public class UserService {
+
+    private UserRepository currentUserRepo;
+    @Autowired
+    public UserService(UserRepository userRepo){
+        currentUserRepo = userRepo;
+    }
+
+    public boolean checkLoginDetails(String mail, String password){
+        String hashedPassword = hashPassword(password);
+        User searchUser = null;
+        List<User> searchUsers = currentUserRepo.findUserForLogin(mail);
+
+        if(searchUsers.size()==1){
+            searchUser=searchUsers.get(0);
+            String dbPasswordHash = searchUser.getPassword();
+            if(hashedPassword==dbPasswordHash){
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public String hashPassword(String password){
+        String hashedPassword = Hashing.sha256()
+                .hashString(password, StandardCharsets.UTF_8)
+                .toString();
+
+        return hashedPassword;
+    }
 }
