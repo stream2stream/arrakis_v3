78bdd17a50a072b589e95eba241fa867480b448d
Mark Muthalaly <markgmuthalaly@gmail.com>
2023-08-03 19:51:26 +0100

    added User model

---
+++ java-api/src/main/java/com/db/grad/javaapi/model/User.java
@@ -0,0 +1,47 @@
+package com.db.grad.javaapi.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    private long id;
+    private String username;
+    private String firstName;
+    private String lastName;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
