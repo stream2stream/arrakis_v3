b13a2a7855a9ebeddbfd6cb0a3142c2f6f9a11c0
hammockryan8 <hammockryan8@gmail.com>
2023-08-03 16:35:02 +0100

    added getters and setters for users

--- java-api/src/main/java/com/db/grad/javaapi/model/User.java
+++ java-api/src/main/java/com/db/grad/javaapi/model/User.java
@@ -12,4 +12,27 @@ public class User {
         this.role = role;
     }
 
+    public String getUserName(){
+        return name;
+    }
+
+    public String getUserEmail(){
+        return email;
+    }
+
+    public String getUserRole(){
+        return role;
+    }
+
+    public void setUserName(String name){
+        this.name = name;
+    }
+
+    public void setUserEmail(String email){
+        this.email = email;
+    }
+
+    public void setUserRole(String role){
+        this.role = role;
+    }
 }
