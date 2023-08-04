eb884668fb17b0b88e67696b4058b2a76567b3f4
unknown <mtalturki42@tntech.edu>
2023-08-04 13:42:44 +0100

    bookcontroller

--- java-api/src/main/java/com/db/grad/javaapi/controller/BookController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/BookController.java
@@ -24,7 +24,7 @@ public class BookController {
     }
 
     @GetMapping("/book")
-    public List <Book> getAllBook() {
+    public List <Book> getAllBooks() {
         return bookService.getAllBooks();
     }
 
