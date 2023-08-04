3544ee3450763486200d62adc1731b1b09c9c0ba
unknown <mtalturki42@tntech.edu>
2023-08-04 15:46:59 +0100

    populated all the tables - no Bookuser java class

--- java-api/src/main/java/com/db/grad/javaapi/service/ICounterPartyService.java
+++ java-api/src/main/java/com/db/grad/javaapi/service/ICounterPartyService.java
@@ -1,6 +1,6 @@
 package com.db.grad.javaapi.service;
 
-import com.db.grad.javaapi.model.Bookuser;
+
 import com.db.grad.javaapi.model.CounterParty;
 
 import java.util.List;
--- java-api/src/main/resources/data.sql
+++ java-api/src/main/resources/data.sql
@@ -31,3 +31,17 @@ INSERT INTO `Bonds` VALUES
 ('sell','USD',300,'23/08/2021','open','23/08/2015','98.76','1.123','USD','87973RAA8',340,'IE00B29LNP31','First Norway Alpha Kl.IV','22/12/2030','active','SOVN','Trading_book_4','Zurich Pension fund 4');
 
 
+
+INSERT INTO book (name) VALUES ('trading_book_1'), ('trading_book_2'), ('Trading_book_3'), ('Trading_book_4'), ('Trading_book_6');
+
+INSERT INTO userr (name, email, role) VALUES ('AZ Holdings Inc', 'azholdings@example.com', 'bond_holder'), ('Acme co', 'acmeco@example.com', 'bond_holder'), ('Sovereign Investments', 'sovereigninvestments@example.com', 'bond_holder'), ('Astra Trading Ltd', 'astratradingltd@example.com', 'bond_holder'), ('Muncipal Gov Of Orange County', 'muncipalgovoforangecounty@example.com', 'bond_holder'), ('Goldman Sachs', 'goldmansachs@example.com', 'bond_holder'), ('UBS', 'ubs@example.com', 'bond_holder'), ('Barclays', 'barclays@example.com', 'bond_holder'), ('British Telecom', 'britishtelecom@example.com', 'bond_holder'), ('Pension Holdings', 'pensionholdings@example.com', 'bond_holder'), ('Zurich Pension fund 4', 'zurichpensionfund4@example.com', 'bond_holder');
+
+INSERT INTO counterparty (name) VALUES ('BNPParibasIssu 4,37% Microsoft Corp (USD)'), ('Airbus 3.15% USD'), ('UBS Facebook (USD)'), ('Amazon'), ('HM Treasury United Kingdon'), ('TEMASEK FINL I LTD GLOBAL MEDIUM TERM NTS BOOK ENTRY REG S'), ('First Norway Alpha Kl.IV');
+
+INSERT INTO book_user (book_id, user_id) VALUES (1, 1), (2, 2), (3, 3), (2, 4), (2, 3), (4, 5), (5, 5), (5, 6), (5, 7), (5, 8), (5, 9), (5, 10), (4, 11);
+
+INSERT INTO security (isin, cusip, issuer_name, maturity_date, coupon, type, face_value, currency, status) VALUES ('XS1988387210', NULL, 'BNPParibasIssu 4,37% Microsoft Corp (USD)', '2021-08-05', 4.37, 'CORP', 1000, 'USD', 'active'), ('USN0280EAR64','123456780','Airbus 3.15% USD','2021-07-30',3.15,'CORP',900,'USD','active'), ('A12356111','123456bh0','UBS Facebook (USD)','2021-09-30',2,'CORP',900,'USD','active'), ('USU02320AG12','AMZN 3.15 08/22/27 REGS','Amazon','2021-08-03',3.15,'CORP',900,'USD','active'), (NULL,'BDCHBW8','HM Treasury United Kingdon','2021-08-09',0.75,'GOVN',900,'GBP','active'), ('US87973RAA86','87973RAA8','TEMASEK FINL I LTD GLOBAL MEDIUM TERM NTS BOOK ENTRY REG S','2021-08-06',2.02,'SOVN',690,'USD','active'), ('IE00B29LNP31','87973RAA8','First Norway Alpha Kl.IV','2030-12-22',1.123,'SOVN',340,'USD','active');
+
+INSERT INTO trades (book_id, security_id, counterparty_id, currency, status, quantity, unit_price, buy_sell, trade_date, settlement_date) VALUES (1, 1, 1, 'USD', 'open', 50, 90, 'buy', '2021-05-13', '2021-08-04'), (1, 1, 1, 'GBP', 'open', 40, 89.56, 'sell', '2021-02-04', '2021-08-04'), (2, 2, 2, 'USD', 'open', 1000, 105.775, 'buy', '2021-05-13', '2021-08-23'), (2, 2, 2, 'GBP', 'open', 900, 105.775, 'sell', '2021-02-04', '2021-09-10'), (3, 3, 3, 'USD', 'open', 50, 90, 'buy', '2021-05-13', '2021-08-23'), (2, 2, 2, 'USD', 'open', 1000, 105.775, 'buy', '2021-05-13', '2021-08-23'), (2, 3, 3, 'USD', 'open', 50, 90, 'sell', '2021-05-13', '2021-08-23');
+
+
