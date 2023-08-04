f792ec8530bdafa2e6b25a082a7dc1610d75f058
biancaserban20 <srbnbianca8@yahoo.com>
2023-08-04 12:13:29 +0300

    Modified db-bonds-data.csv again

--- java-api/src/main/java/com/db/grad/javaapi/repository/SecurityRepository.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/SecurityRepository.java
@@ -4,8 +4,12 @@ import com.db.grad.javaapi.model.Security;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface SecurityRepository extends JpaRepository<Security, Integer>
 {
+    List<Security> findByDate(Date date);
 
 }
--- java-api/src/main/java/com/db/grad/javaapi/service/SecurityHandler.java
+++ java-api/src/main/java/com/db/grad/javaapi/service/SecurityHandler.java
@@ -5,6 +5,7 @@ import com.db.grad.javaapi.repository.SecurityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
