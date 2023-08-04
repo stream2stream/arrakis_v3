6aba6560f60c3793242cbacf00bc4847701d6d2f
robertsamata <robert.samata@s.unibuc.ro>
2023-08-04 10:28:07 +0100

    login update

--- java-api/src/main/java/com/db/grad/javaapi/controller/UsersController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/UsersController.java
@@ -1,30 +1,35 @@
 package com.db.grad.javaapi.controller;
 
+import com.db.grad.javaapi.dto.LoginDTO;
 import com.db.grad.javaapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/")
 @CrossOrigin(origins = "http://localhost:3000")
 public class UsersController {
 
     private UserService usersService;
+
     @Autowired
-    public UsersController(UserService us){
-        usersService=us;
+    public UsersController(UserService us) {
+        usersService = us;
     }
 
-    @GetMapping("/check_login/{username}/{password}")
-    public ResponseEntity<String> checkLoginDetails(@PathVariable String username, @PathVariable String password){
+    @PostMapping("/check_login")
+    public ResponseEntity<String> checkLoginDetails(@RequestBody LoginDTO loginRequest) {
+        String username = loginRequest.getUsername();
+        String password = loginRequest.getPassword();
+
         boolean valid = usersService.checkLoginDetails(username, password);
-        if(valid)
-        {
+        if (valid) {
             return ResponseEntity.status(HttpStatus.OK).body("Login credential verification is successful.\n");
-        }
-        else {
+        } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Login credential verification failed.\n");
         }
     }
---
+++ java-api/src/main/java/com/db/grad/javaapi/dto/LoginDTO.java
@@ -0,0 +1,24 @@
+package com.db.grad.javaapi.dto;
+
+public class LoginDTO {
+    private String username;
+    private String password;
+
+    // Getters and setters (you can use your IDE to generate them)
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
