1301dbc5ff5a0267dffe3bbef100fba24f55b310
Thin Yati Su <thinyatihsu.hsu@gmail.com>
2023-08-04 20:45:06 +0100

    "In the mid of add a date picker"

--- react-app/src/components/AllMaturingBonds.jsx
+++ react-app/src/components/AllMaturingBonds.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MaturingBond from "./MaturingBond";
 import moment from "moment";
+import DatePicker from "react-datepicker";
 
 const AllMaturingBonds = (props) => {
   //Hardcoded date, later user can select
@@ -8,6 +9,7 @@ const AllMaturingBonds = (props) => {
   var arr = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
 
   return arr.map((num) => (
+    
     <MaturingBond
       info={{date: moment(date, "YYYY-MM-DD").add(num, "d").toDate(), check: props.info}}
       key={num}
