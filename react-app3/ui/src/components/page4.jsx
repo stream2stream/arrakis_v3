04430d514f03f911658658def245598767e968f9
lchapata <lchapata@trincoll.edu>
2023-08-04 16:50:36 +0100

    tables

--- react-app3/ui/src/components/page4.jsx
+++ react-app3/ui/src/components/page4.jsx
@@ -1,7 +1,73 @@
-import React from 'react'
+import React from "react";
 
-export default function page4() {
+const Page4 = () => {
+  const columns = [
+    "ISIN",
+    "Type",
+    "Issuer",
+    "Maturity",
+    "Face Value",
+    "Currency",
+    "Coupon %",
+    "Status",
+  ];
+
+  const data = [
+    {
+      ISIN: "US1234567890",
+      Type: "Bond",
+      Issuer: "ABC Corp",
+      Maturity: "2025-08-01",
+      "Face Value": "1000",
+      Currency: "USD",
+      "Coupon %": "5.00",
+      Status: "Active",
+    },
+    {
+      ISIN: "GB9876543210",
+      Type: "Equity",
+      Issuer: "XYZ Inc",
+      Maturity: "N/A",
+      "Face Value": "N/A",
+      Currency: "GBP",
+      "Coupon %": "N/A",
+      Status: "Inactive",
+    },
+   
+  ];
+
   return (
-    <div>page4</div>
-  )
-}
+    <div>
+      <h2>Page4</h2>
+      <table>
+        <thead>
+          <tr>
+            {columns.map((column, index) => (
+              <th key={index}>{column}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {columns.map((column) => (
+              <td key={column}>{row[column]}</td>
+            ))}
+          </tr>
+        ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Page4;
+
+
+// You can add data to the table body dynamically later using state, props, or by fetching data from an API, depending on your application's needs.
+
+
+
+
+
+
