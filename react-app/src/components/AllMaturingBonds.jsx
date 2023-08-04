6e92bd3a6438c459846de1832282ae15c6ff33d5
Thin Yati Su <thinyatihsu.hsu@gmail.com>
2023-08-04 15:09:56 +0100

    in the mid of creating maturity table

--- react-app/src/components/AllMaturingBonds.jsx
+++ react-app/src/components/AllMaturingBonds.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import MaturingBond from "./MaturingBond";
+import MaturingBondsList from './MaturingBondsList';
+// import MaturingBond from "./MaturingBond";
 import moment from "moment";
 
-const AllMaturingBonds = () => {
+const AllMaturingBonds = (dateBonds) => {
   //Hardcoded date, later user can select
   var date = "2020/01/01";
   var arr = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
@@ -14,6 +15,7 @@ const AllMaturingBonds = () => {
   };
 
   return arr.map((num) => (
+
     <table className="table table-striped">
       <thead>
         <tr>
@@ -27,21 +29,8 @@ const AllMaturingBonds = () => {
           <th scope="col">Status</th>
         </tr>
       </thead>
-      <tbody>
-        {dateBonds.map((row, index) => (
-          <tr key={index}>
-            <td>{row.isin}</td>
-            <td>{row.type}</td>
-            <td>{row.issuerName}</td>
-            <td>{formatDate(row.bondMaturityDate)}</td>
-            <td>{row.faceValue}</td>
-            <td>{row.bondCurrency}</td>
-            <td>{row.couponPercent}</td>
-            <td>{row.status}</td>
-          </tr>
-        ))}
-      </tbody>
     </table>
+
     // <MaturingBond
     //   info={moment(date, "YYYY-MM-DD").add(num, "d").toDate()}
     //   key={num}
