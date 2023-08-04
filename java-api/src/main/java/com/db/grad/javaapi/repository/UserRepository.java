8b94c2b05db41c0ecb00666580f5ca9e2b04a23d
hammockryan8 <hammockryan8@gmail.com>
2023-08-04 14:30:06 +0100

    finding all users

--- java-api/src/main/java/com/db/grad/javaapi/repository/UserRepository.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/UserRepository.java
@@ -3,12 +3,18 @@ package com.db.grad.javaapi.repository;
 import com.db.grad.javaapi.model.Bond;
 import com.db.grad.javaapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-//    List<User> findAllUsers();
+
+    @Query(nativeQuery = true, value = "select * from users")
+    List<User> findAllUsers();
+
+    @Query(nativeQuery = true, value = "select * from users where name = :name")
+    User findUser(String name);
 
 }
