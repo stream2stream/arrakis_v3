da667251b276cee0605c4a372b966f7c0a60b498
Mario <marioirimia@gmail.com>
2023-08-04 16:53:19 +0300

    ZoomBond Page Responsive

--- react-app/src/pages/ZoomBond.jsx
+++ react-app/src/pages/ZoomBond.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import ZoomBondTable from "../components/ZoomBondTable";
 import ZoomTradeTable from "../components/ZoomTradeTable";
 import Box from '@mui/material/Box';
 import '../App.css';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
@@ -20,33 +20,46 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Grid from '@mui/material/Grid';
+import TableContainer from '@mui/material/TableContainer';
+import Paper from '@mui/material/Paper';
 import Row from "react-bootstrap/Row";
 
 const ZoomBond = () => {
-  const [expanded, setExpanded] = React.useState(false);
   const [selectedISIN, setSelectedISIN] = useState('');
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
+
   const handleRowClick = (isin) => {
     setSelectedISIN(isin);
   };
+
   return (
     <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
       <Toolbar />
       <Row className="row">
-        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-          <div style={{ margin: "20px 0" }}>BOND TABLE</div>
-
-          {/* Pass onRowClick prop to ZoomBondTable */}
-          <ZoomBondTable onRowClick={handleRowClick} />
-
-          <div style={{ margin: "20px 0" }}>TRADE TABLE</div>
-
-          {/* Pass isin prop to ZoomTradeTable */}
-          <ZoomTradeTable isin={selectedISIN} />
-
-        </div>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Card>
+              <CardHeader title="BOND TABLE" />
+              <CardContent>
+                <TableContainer component={Paper} sx={{ overflowX: 'auto' }}>
+                  {/* Pass onRowClick prop to ZoomBondTable */}
+                  <ZoomBondTable onRowClick={handleRowClick} />
+                </TableContainer>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12}>
+            <Card>
+              <CardHeader title="TRADE TABLE" />
+              <CardContent>
+                <TableContainer component={Paper} sx={{ overflowX: 'auto' }}>
+                  {/* Pass isin prop to ZoomTradeTable */}
+                  <ZoomTradeTable isin={selectedISIN} />
+                </TableContainer>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
       </Row>
     </Box>
   );
