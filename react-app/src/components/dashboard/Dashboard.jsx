453d37fb96c8edbefe86411638394e3f7c4a4d89
propixel2741 <propixel2741@gmail.com>
2023-08-04 15:02:57 -0400

    User can now see all bonds active in system (Front End)

--- react-app/src/components/dashboard/Dashboard.jsx
+++ react-app/src/components/dashboard/Dashboard.jsx
@@ -4,56 +4,6 @@ import { Button } from "@mui/material";
 import { getAllTrades } from "../../services/DashboardService";
 
 export const Dashboard = () => {
-    /*function formatJSON(currency,quantity,unit_price,buy_sell,book_name,counter_party_name,isin,cusip,issuer_name,maturity_date,coupon,type,face_value,security_currency,status,trade_date,settlement_date) {
-        var formattedData = [],
-        const jsonList = Object.values(data).map(({currency,quantity,unit_price,buy_sell,book_name,counter_party_name,isin,cusip,issuer_name,maturity_date,coupon,type,face_value,security_currency,status,trade_date,settlement_date}) => (
-
-        ))
-    }*/
-
-    /*function formatJSON(data) {
-        const formattedData = [];
-        for (const jsonString of data) {
-            try {
-                const parsedObject = JSON.parse(jsonString);
-                formattedData.push(parsedObject);
-            } catch {
-                console.error("Error parsing JSON: ", error);
-            }
-        }
-        return formattedData;
-    }
-
-    const parsedObjects = formatJSON(data);
-
-    const currency = 
-    {field: "quantity
-    {field: "unit_price
-    {field: "book_name
-    {field: "counter_party_name
-    {field: "isin
-    {field: "cusip
-    {field: "issuer_name
-    {field: "maturity_date
-    {field: "coupon
-    {field: "type
-    {field: "face_value
-    {field: "security_currency
-    {field: "status
-    {field: "trade_date
-    {field: "settlement_date*/
-
-    /*function parseJSONObjects(data) {
-        const dataList = [];
-        for (const jsonString of data) {
-            try {
-                const parsedObject = JSON.parse(jsonString);
-                const row = [parsedObject]
-            } catch {
-
-            }
-        }
-    }*/
 
     const [data, setData] = useState([]);
 
@@ -69,13 +19,39 @@ export const Dashboard = () => {
               });
     }, []);
 
-    
-
     const rows = [
-        {id: data.id, currency: data.currency,quantity: 1,unit_price: 900,buy_sell: "BUY",book_name: "TRADING_BOOK_X",counter_party_name: "ABC International",isin: "asduhasd",cusip: "12x812s2",issuer_name: "SomeCorporation",maturity_date: "2025-03-21",coupon: "5.00",type: "CORP",face_value: 1000,security_currency: "USD",status: "ACTIVE",trade_date: "2023-07-03",settlement_date: "2023-07-01"}
+        
     ];
 
+    function jsonParseToRow(jsonObject, uniqueId) {
+        return {
+            id: uniqueId,
+            currency: jsonObject.currency,
+            quantity: jsonObject.quantity,
+            unit_price: jsonObject.unit_price,
+            buy_sell: jsonObject.buy_sell,
+            book_name: jsonObject.book.name,
+            counter_party_name: jsonObject.counterParty.name,
+            isin: jsonObject.security.isin,
+            cusip: jsonObject.security.cusip,
+            issuer_name: jsonObject.security.issuer_name,
+            maturity_date: jsonObject.security.maturity_date,
+            coupon: jsonObject.security.coupon,
+            type: jsonObject.security.type,
+            face_value: jsonObject.security.face_value,
+            security_currency: jsonObject.security.currency,
+            status: jsonObject.security.status,
+            trade_date: jsonObject.trade_date,
+            settlement_date: jsonObject.settlement_date,
+        }
+    }
+
+    for (var i = 0; i < data.length; i++) {
+        rows.push(jsonParseToRow(data[i], i+1));
+    }
+
     const cols = [
+        {field: "id", headerName: "ID", width: 150},
         {field: "currency", headerName: "Currency", width: 150},
         {field: "quantity", headerName: "Quantity", width: 150},
         {field: "unit_price", headerName: "Unit Price", width: 150},
@@ -101,7 +77,7 @@ export const Dashboard = () => {
         <>
         <h1>Bond Dashboard</h1>
         <div>
-            <DataGrid rows={rows} columns={cols}
+            <DataGrid rows={rows} columns={cols} getRowId={(row) => row.id}
                 sx={{
                     m:5,
                     boxShadow: 2,
