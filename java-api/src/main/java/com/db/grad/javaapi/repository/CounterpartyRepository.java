71ea5503ecfa34157bb35427006e13e06ae5e587
monikazeng <monikaqz12@gmail.com>
2023-08-04 14:10:28 -0400

    updates to repo

---
+++ java-api/src/main/java/com/db/grad/javaapi/repository/CounterpartyRepository.java
@@ -0,0 +1,4 @@
+package com.db.grad.javaapi.repository;
+
+public interface CounterpartyRepository {
+}
--- java-api/src/main/java/com/db/grad/javaapi/repository/SecurityRepository.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/SecurityRepository.java
@@ -1,28 +1,36 @@
-//package com.db.grad.javaapi.repository;
-//
+package com.db.grad.javaapi.repository;
+
 //import com.db.grad.javaapi.model.Dog;
-//import com.db.grad.javaapi.model.Security;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.List;
-//
-//@Repository
-//public interface SecurityRepository extends JpaRepository<Security,Integer> {
-//
-//    // mvp 1.0 story 1
-//    @Query(nativeQuery = true, value = "select * from Security where Status = 'active'")
-//    List<Security> findActiveBonds();
-//
-//    // mvp 1.0 story 2
-//    @Query(nativeQuery = true, value = "")
-//
-//
-//    // mvp 1.0 story 5
-//    @Query(nativeQuery = true, value ="select issuer from Security where ID= :id")
-//    List<Security> findIssuerfromBondID(Integer id);
-//
-//
-//
-//}
+import com.db.grad.javaapi.model.Security;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface SecurityRepository extends JpaRepository<Security,Integer> {
+
+    // mvp 1.0 story 1
+    @Query(nativeQuery = true, value = "select * from Security where Status = 'active'")
+    List<Security> findActiveBonds();
+
+    // mvp 1.0 story 2
+    @Query(nativeQuery = true, value = "SELECT * FROM SECURITY WHERE MATURITY_DATE BETWEEN DATEADD('DAY', -5, :USER_DATE) AND DATEADD('DAY', 5, :USER_DATE);")
+    List<Security> find5DaysBonds(LocalDateTime USER_DATE);
+
+
+    // mvp 1.0 story 3
+
+
+    // mvp 1.0 story 4
+
+
+    // mvp 1.0 story 5
+    @Query(nativeQuery = true, value ="select issuer from Security where ID= :id")
+    List<Security> findIssuerfromBondID(Integer id);
+
+
+
+}
