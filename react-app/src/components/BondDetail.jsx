17ebbf55f5d3c33af3d9cb47797eea862bc3e3f1
cas <ca6309906@gmail.com>
2023-08-04 15:09:34 +0100

    table files

--- react-app/src/components/BondDetail.jsx
+++ react-app/src/components/BondDetail.jsx
@@ -3,17 +3,18 @@ import "../App.css";
 
 const BondDetail = (props) => {
   return (
-    <></>
-    // <div className="card my-3">
-    //   <div className="card-body">
-    //     <h1 className="card-title">{props.info.issuerName}</h1>
-    //     <p className="card-text">
-    //       Face Value: {props.info.faceValue} &emsp; Coupon:{" "}
-    //       {props.info.couponPercent} &emsp; Maturity Date:{" "}
-    //       {props.info.bondMaturityDate} &emsp; ISIN: {props.info.isin}
-    //     </p>
-    //   </div>
-    // </div>
+    <>
+    <div className="card my-3">
+      <div className="card-body">
+        <h1 className="card-title">{props.info.issuerName}</h1>
+        <p className="card-text">
+          Face Value: {props.info.faceValue} &emsp; Coupon:{" "}
+          {props.info.couponPercent} &emsp; Maturity Date:{" "}
+          {props.info.bondMaturityDate} &emsp; ISIN: {props.info.isin}
+        </p>
+      </div>
+    </div>
+    </>
   );
 };
 
---
+++ react-app/src/components/MatureBondsWrapper.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AllMaturingBonds from './AllMaturingBonds'
+
+const MatureBondsWrapper = () => {
+  return (
+    <AllMaturingBonds/>
+  )
+}
+
+export default MatureBondsWrapper
