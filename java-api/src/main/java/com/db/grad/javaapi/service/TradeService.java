4f18dcc2bf9441807df9a91f29fddc0a47ff5e5a
ancamariac <ciuciuancamaria@gmail.com>
2023-08-04 15:33:19 +0300

    Bonds controller & bond current position by ID

--- java-api/src/main/java/com/db/grad/javaapi/service/TradeService.java
+++ java-api/src/main/java/com/db/grad/javaapi/service/TradeService.java
@@ -1,9 +1,14 @@
 package com.db.grad.javaapi.service;
 
 import com.db.grad.javaapi.model.Trade;
+import com.db.grad.javaapi.utils.Triple;
+import com.google.cloud.Tuple;
 
 import java.util.List;
+import java.util.Map;
 
 public interface TradeService {
     List<Trade> getAllTradesByISIN(String ISIN);
+
+    Triple<String, Integer, Double> getStatsByBondHolderID(int id);
 }
