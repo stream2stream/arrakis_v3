3b9ffb71605c6b9bccee3561fda5e2a5a4f781bf
Pulkit Khera <pulkitkhera97@gmail.com>
2023-08-04 14:27:57 +0100

    Added CORS

--- java-api/src/main/java/com/db/grad/javaapi/controller/SecurityController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/SecurityController.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(origins = "http://localhost:3000")
 public class SecurityController {
     @Autowired
     private SecurityService securityService;
