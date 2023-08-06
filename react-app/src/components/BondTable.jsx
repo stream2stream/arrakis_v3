2a11d85f508e7289c118a03e86432ee10f96d05d
Shruthi H R <shruthi2602@outlook.com>
2023-08-06 13:23:45 +0200

    added comment

--- react-app/src/components/BondTable.jsx
+++ react-app/src/components/BondTable.jsx
@@ -1,13 +1,11 @@
 
-import React from 'react';
-import { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.css";
-import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
+import React from 'react';
 import BootstrapTable from "react-bootstrap-table-next";
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 import paginationFactory from "react-bootstrap-table2-paginator";
 
-
-
+//added something
 
 const columns = [
   {
