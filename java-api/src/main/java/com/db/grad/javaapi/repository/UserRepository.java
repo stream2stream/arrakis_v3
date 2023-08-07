9831f53e39ee3f25c3d34f2c78439915ae7d866f
hammockryan8 <hammockryan8@gmail.com>
2023-08-07 18:31:03 +0100

    deleting dead code

--- java-api/src/main/java/com/db/grad/javaapi/repository/UserRepository.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.db.grad.javaapi.repository;
 
-import com.db.grad.javaapi.model.Bond;
 import com.db.grad.javaapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
--- java-api/src/main/java/com/db/grad/javaapi/service/ITradeHandler.java
+++ java-api/src/main/java/com/db/grad/javaapi/service/ITradeHandler.java
@@ -1,10 +1,8 @@
 package com.db.grad.javaapi.service;
 
-import com.db.grad.javaapi.model.Bond;
 import com.db.grad.javaapi.model.Trade;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ITradeHandler
 {
