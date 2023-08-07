bd821113031365c26270f3a38b5f2c1c1bb6f0b9
elsasjogren <elsasjogren5@gmail.com>
2023-08-07 14:51:45 +0100

    new branch

--- java-api/src/main/java/com/db/grad/javaapi/repository/TradeRepository.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/TradeRepository.java
@@ -21,14 +21,11 @@ public interface TradeRepository extends JpaRepository<Trade, Long> {
     @Query(nativeQuery = true, value = "select security.maturity_date FROM trades JOIN security on trades.book_id = :id WHERE trades.security_id = security.id")
     List<String> getMaturityDates (long id);
 
-    // @Query(nativeQuery = true, value = "SELECT type, issuer, face_value, coupon_percent, status, client, trade_status, quantity, unit_price, trade_currency, buy/sell, trade_date, settlement_date FROM Trades t JOIN Security s ON t.book_id = :id AND s.id = t.security_id AND s.maturity_date > (SELECT CURRENT_TIMESTAMP)")
-    // List<Trade> getTradesBeforeMaturity (long id);
+    // @Query(nativeQuery = true, value = "SELECT status, currency, face_value, 
+    // type coupon, issuer_name from security s natural join Trades t on t. where booking_id = :id secuirty_id = s.id")
+    // List<Object> getSecurityInfo (long id);
 
-    @Query(nativeQuery = true, value = "SELECT status, currency, face_value, type coupon, issuer_name from security")
-    List<Object> getSecurityInfo (long id);
-
-    @Query(nativeQuery = true, value = "SELECT currency, status, quantity, unit_price, buy_sell, trade_date, settlement_date from trades")
-    List<Object> getTradesInfo (long id);
-
+    // @Query(nativeQuery = true, value = "SELECT currency, status, quantity, unit_price, buy_sell, trade_date, settlement_date from trades where booking_id = :id")
+    // List<Object> getTradesInfo (long id);
 
 }
