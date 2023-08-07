f94b6018d13099d01d27ffca9f27d8f463bd8cb4
elisa-idita <elisa.idita@gmail.com>
2023-08-07 11:35:45 +0100

    created tests for nextFiveDays and previousFiveDays methods

--- java-api/src/test/java/com/db/grad/javaapi/service/SecurityServiceTest.java
+++ java-api/src/test/java/com/db/grad/javaapi/service/SecurityServiceTest.java
@@ -15,6 +15,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import java.lang.reflect.Array;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -31,26 +32,40 @@ class SecurityServiceTest {
         MockitoAnnotations.initMocks(this);
 
         List<Security> mockSecurities = Arrays.asList(
-                new Security(10, "test1", "test11", "test_name1", LocalDateTime.of(2021, 8, 10, 0, 0, 0), 2, "type1", 16, "currency1", "active"),
-                new Security(11, "test2", "test22", "test_name2", LocalDateTime.of(2021, 8, 12, 0, 0, 0), 2, "type2", 8, "currency2", "inactive")
+                new Security(10, "test1", "test11", "test_name1", LocalDateTime.of(2021, 8, 1, 0, 0, 0), 2, "type1", 16, "currency1", "active"),
+                new Security(11, "test2", "test22", "test_name2", LocalDateTime.of(2021, 8, 7, 0, 0, 0), 2, "type2", 8, "currency2", "inactive")
         );
 
-        Mockito.when(securityRepository.getActiveBonds()).thenReturn(mockSecurities);
+        List<Security> activeBonds = new ArrayList<>();
+        activeBonds.add(mockSecurities.get(0));
 
+        List<Security> nextFiveDays = new ArrayList<>();
+        nextFiveDays.add(mockSecurities.get(1));
+
+        List<Security> previousFiveDays = new ArrayList<>();
+        previousFiveDays.add(mockSecurities.get(0));
+
+        Mockito.when(securityRepository.getActiveBonds()).thenReturn(activeBonds);
+        Mockito.when(securityRepository.getBondsForNextFiveDays()).thenReturn(nextFiveDays);
+        Mockito.when(securityRepository.getBondsForPreviousFiveDays()).thenReturn(previousFiveDays);
     }
 
     @Test
     public void testGetActiveBonds() {
         List<Security> activeSecurities = securityService.getBonds();
-
-        Assert.assertEquals(2, activeSecurities.size());
+        Assert.assertEquals(1, activeSecurities.size());
     }
 
     @Test
-    void getBondsForNextFiveDays() {
+    void testGetBondsForNextFiveDays() {
+        List<Security> nextFiveDaysSecurities = securityService.getBondsForNextFiveDays();
+        Assert.assertEquals(1, nextFiveDaysSecurities.size());
+
     }
 
     @Test
-    void getBondsForPreviousFiveDays() {
+    void testGetBondsForPreviousFiveDays() {
+        List<Security> previousFiveDaysSecurities = securityService.getBondsForPreviousFiveDays();
+        Assert.assertEquals(1, previousFiveDaysSecurities.size());
     }
 }
