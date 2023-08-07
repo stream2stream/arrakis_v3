4303e0914ea76a0949a26f76e3a27566ba05c602
calkat <cjkathman@gmail.com>
2023-08-07 11:44:55 -0400

    Adding in the CUSIP to the details page information

--- java-api/src/main/java/com/db/grad/javaapi/repository/TradeRepository.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/TradeRepository.java
@@ -21,7 +21,7 @@ public interface TradeRepository extends JpaRepository<Trade, Long> {
     @Query(nativeQuery = true, value = "select security.maturity_date FROM trades JOIN security on trades.book_id = :id WHERE trades.security_id = security.id")
     List<String> getMaturityDates (long id);
 
-    @Query(nativeQuery = true, value = "SELECT s.maturity_date, s.isin, s.currency, s.face_value, s.type, s.coupon, s.issuer_name, t.currency as t_curr, t.status, t.quantity, t.unit_price, t.buy_sell, t.trade_date, t.settlement_date from security s JOIN Trades t ON t.security_id = s.id AND t.book_id = :id")
+    @Query(nativeQuery = true, value = "SELECT s.maturity_date, s.isin, s.currency, s.face_value, s.type, s.coupon, s.issuer_name, t.currency as t_curr, t.status, t.quantity, t.unit_price, t.buy_sell, t.trade_date, t.settlement_date, s.cusip from security s JOIN Trades t ON t.security_id = s.id AND t.book_id = :id")
     List<Object> getSecurityInfo (long id);
 
     @Query(nativeQuery = true, value = "SELECT currency, status, quantity, unit_price, buy_sell, trade_date, settlement_date from trades where book_id = :id")
--- react-app/src/components/BondDetailPage.jsx
+++ react-app/src/components/BondDetailPage.jsx
@@ -20,7 +20,7 @@ const BondDetailPage = (props) => {
             let results = {ISIN: res2.data[i][1], Type: res2.data[i][4], Issuer: res2.data[i][6], Maturity: res2.data[i][0].substring(0,10), FaceValue: res2.data[i][3],
                            BondCurrency: res2.data[i][2], Coupon: res2.data[i][5], Status: res2.data[i][8], BookID: res.data, Quantity: res2.data[i][9], UnitPrice: res2.data[i][10],
                            TradeCurrency: res2.data[i][7], Buysell: res2.data[i][11], TradeDate: res2.data[i][12].substring(0,10), SettlementDate: res2.data[i][13].substring(0,10),
-                           TradeStatus: res2.data[i][8]};
+                           TradeStatus: res2.data[i][8], Cusip: res2.data[i][14]};
             finalData[i] = results;
           }
         }
@@ -74,6 +74,7 @@ const BondDetailPage = (props) => {
         <table style = {TableStyle}>
           <tr>
             <th style = {THStyle}> ISIN </th>
+            <th style = {THStyle}> CUSIP </th>
             <th style = {THStyle}> Type </th>
             <th style = {THStyle}> Issuer </th>
             <th style = {THStyle}> Maturity </th>
@@ -86,6 +87,7 @@ const BondDetailPage = (props) => {
             {data.map(bond =>
                 <tr>
                     <td style={TDStyle}>{bond["ISIN"]}</td>
+                    <td style={TDStyle}>{bond["Cusip"]}</td>
                     <td style={TDStyle}>{bond["Type"]}</td>
                     <td style={TDStyle}>{bond["Issuer"]}</td>
                     <td style={TDStyle}>{bond["Maturity"]}</td>
